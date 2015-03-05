69dfb7b485ed1ee9a3c443f00aaebed39c5779d9
Settapak Yeerunsiri <settapak@gmail.com>
2015-03-05 08:16:31 +0700

    Implement CustomViewGroup NumberButton
    - Implemnet CustomViewGroup NumberButton

--- app/src/main/java/com/zaapamstudio/matchchallenge/view/NumberButton.java
+++ app/src/main/java/com/zaapamstudio/matchchallenge/view/NumberButton.java
@@ -3,6 +3,7 @@ package com.zaapamstudio.matchchallenge.view;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.zaapamstudio.matchchallenge.R;
@@ -11,6 +12,11 @@ import com.zaapamstudio.matchchallenge.R;
  * Created by suthamaskamollasal on 3/3/15 AD.
  */
 public class NumberButton extends LinearLayout {
+
+    private ImageView imgNumber1;
+    private ImageView imgNumber2;
+    private int currentNumber;
+
     public NumberButton(Context context) {
         super(context);
 
@@ -34,5 +40,15 @@ public class NumberButton extends LinearLayout {
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.view_number, this, true);
 
+        imgNumber1 = (ImageView) findViewById(R.id.imgNumber1);
+        imgNumber2 = (ImageView) findViewById(R.id.imgNumber2);
+    }
+
+    public void setNumber(int number) {
+        String str = Integer.toString(number);
+    }
+
+    public int getNumber() {
+        return currentNumber;
     }
 }
